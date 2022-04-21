07e7ed076c69dfa245880429f0d203221f18dec2
Denis Lesnov <denis@lesnov.me>
2022-04-21 23:41:44 +0300

    Updates

---
+++ precache-manifest.36d304fef3571f885f661922cb9a34c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/a7n/favicon.ico"
+  },
+  {
+    "revision": "0e7bffa4ed351631abb3cb49516ddf04",
+    "url": "/a7n/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/a7n/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/a7n/manifest.json"
+  },
+  {
+    "revision": "2d10fff2ea610cb11672",
+    "url": "/a7n/static/css/main.db17225b.chunk.css"
+  },
+  {
+    "revision": "2d10fff2ea610cb11672",
+    "url": "/a7n/static/js/main.2d10fff2.chunk.js"
+  },
+  {
+    "revision": "3e2a5d529d2d0f720bdc",
+    "url": "/a7n/static/js/runtime~main.3e2a5d52.js"
+  },
+  {
+    "revision": "85be547f8570d4ea2aec",
+    "url": "/a7n/static/js/vendors~main.85be547f.chunk.js"
+  }
+]);
